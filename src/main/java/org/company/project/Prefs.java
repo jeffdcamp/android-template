d12f331cdb10b7f82b1d25137563720fde3ce13b
Jeff Campbell <jeffdcamp@gmail.com>
2013-04-28 19:41:57 -0600

    Added new actionbar icon Improved Prefs

--- src/main/java/org/company/project/Prefs.java
+++ src/main/java/org/company/project/Prefs.java
@@ -1,12 +1,17 @@
 package org.company.project;
 
-/**
- * 
- * @author jcampbell
- */
+import android.content.SharedPreferences;
+import com.google.inject.Singleton;
+
+import javax.inject.Inject;
+
+@Singleton
 public final class Prefs {
 
     public static final String PREF_SOMETHING = "pref_enable_something_id";
+
+    @Inject
+    private SharedPreferences preferences;
 
     private Prefs() {
     }
