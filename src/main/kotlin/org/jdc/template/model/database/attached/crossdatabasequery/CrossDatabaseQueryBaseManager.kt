30721050d4489280971c89d38150c180e4ea1e7d
Jeff Campbell <jeffdcamp@gmail.com>
2016-05-07 20:22:38 -0600

    DBTools 7.0.0 added copy() to BaseRecords Version fixes

--- src/main/kotlin/org/jdc/template/model/database/attached/crossdatabasequery/CrossDatabaseQueryBaseManager.kt
+++ src/main/kotlin/org/jdc/template/model/database/attached/crossdatabasequery/CrossDatabaseQueryBaseManager.kt
@@ -9,9 +9,9 @@
 
 package org.jdc.template.model.database.attached.crossdatabasequery
 
-import org.dbtools.android.domain.RxKotlinAndroidBaseManagerReadOnly
-import org.dbtools.android.domain.database.DatabaseWrapper
 import org.jdc.template.model.database.DatabaseManager
+import org.dbtools.android.domain.database.DatabaseWrapper
+import org.dbtools.android.domain.RxKotlinAndroidBaseManagerReadOnly
 
 
 @SuppressWarnings("all")
