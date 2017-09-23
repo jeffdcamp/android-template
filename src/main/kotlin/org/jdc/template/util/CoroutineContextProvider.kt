533a6b3c8fd2f98cbfa3fb5569c36ebfffe7838d
Jeff Campbell <jeffdcamp@gmail.com>
2017-09-23 09:59:55 -0600

    Improved CoroutineContextProvider to show stacktraces while in a coroutine

--- src/main/kotlin/org/jdc/template/util/CoroutineContextProvider.kt
+++ src/main/kotlin/org/jdc/template/util/CoroutineContextProvider.kt
@@ -1,8 +1,12 @@
 package org.jdc.template.util
 
+import kotlinx.coroutines.experimental.CancellationException
 import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.CoroutineExceptionHandler
+import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.newFixedThreadPoolContext
+import kotlinx.coroutines.experimental.newSingleThreadContext
 import kotlin.coroutines.experimental.CoroutineContext
 
 interface CoroutineContextProvider {
@@ -12,22 +16,46 @@ interface CoroutineContextProvider {
 
     object MainCoroutineContextProvider : CoroutineContextProvider {
         override val ui: CoroutineContext
-            get() = UI
+            get() = UI + defaultCoroutineExceptionHandler
         override val network: CoroutineContext
-            get() = newFixedThreadPoolContext(2, "network-context")
+            get() = newFixedThreadPoolContext(2, "network-context") + defaultCoroutineExceptionHandler
         override val commonPool: CoroutineContext
-            get() = CommonPool
+            get() = CommonPool + defaultCoroutineExceptionHandler
 
     }
 
     object TestCoroutineContextProvider : CoroutineContextProvider {
         override val ui: CoroutineContext
-            get() = CommonPool
+            get() = CommonPool + defaultCoroutineExceptionHandler
         override val network: CoroutineContext
-            get() = CommonPool
+            get() = CommonPool + defaultCoroutineExceptionHandler
         override val commonPool: CoroutineContext
-            get() = CommonPool
+            get() = CommonPool + defaultCoroutineExceptionHandler
+    }
 
+    object TestJDBCCoroutineContextProvider : CoroutineContextProvider {
+        private val singleTreadCoroutineContext = newSingleThreadContext("Test Context")
+
+        override val ui: CoroutineContext
+            get() = singleTreadCoroutineContext + defaultCoroutineExceptionHandler
+        override val network: CoroutineContext
+            get() = singleTreadCoroutineContext + defaultCoroutineExceptionHandler
+        override val commonPool: CoroutineContext
+            get() = singleTreadCoroutineContext + defaultCoroutineExceptionHandler
+    }
+
+    companion object {
+        val defaultCoroutineExceptionHandler = CoroutineExceptionHandler { coroutineContext, exception ->
+            // Copied from CoroutineExceptionHandler Except the first few lines to prevent infinite recursion
+
+            // ignore CancellationException (they are normal means to terminate a coroutine)
+            if (exception is CancellationException) return@CoroutineExceptionHandler
+            // quit if successfully pushed exception as cancellation reason
+            if (coroutineContext[Job]?.cancel(exception) == true) return@CoroutineExceptionHandler
+            // otherwise just use thread's handler
+            val currentThread = Thread.currentThread()
+            currentThread.uncaughtExceptionHandler
+        }
     }
 }
 
