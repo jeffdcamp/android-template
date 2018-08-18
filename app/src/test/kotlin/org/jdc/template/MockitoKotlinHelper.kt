4925d09789f18f4c24292edff5632a96b8d30f2e
Jeff Campbell <jeffdcamp@gmail.com>
2018-08-18 09:07:54 -0600

    Minor comment update on MockitoKotlinHelper

--- app/src/test/kotlin/org/jdc/template/MockitoKotlinHelper.kt
+++ app/src/test/kotlin/org/jdc/template/MockitoKotlinHelper.kt
@@ -5,6 +5,7 @@ import org.mockito.Mockito
 object MockitoKotlinHelper {
     // Helper to handle Non-null any()  (Mockito does not handle Kotlin non-null objects with any())
     // https://medium.com/@elye.project/befriending-kotlin-and-mockito-1c2e7b0ef791#.10oes9nnp
+    // Example: MockitoKotlinHelper.any<URI>()
     fun <T> any(): T {
         Mockito.any<T>()
         return uninitialized()
