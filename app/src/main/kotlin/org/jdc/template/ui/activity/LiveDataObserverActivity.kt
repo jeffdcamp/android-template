52cc4be32fdf8795f4c0accf16086c42f8833a49
jeffdcamp <jeffdcamp@gmail.com>
2019-08-21 21:45:59 -0600

    Improved Activity/Fragment observe methods

--- app/src/main/kotlin/org/jdc/template/ui/activity/LiveDataObserverActivity.kt
+++ app/src/main/kotlin/org/jdc/template/ui/activity/LiveDataObserverActivity.kt
@@ -5,11 +5,11 @@ import androidx.lifecycle.Observer
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class LiveDataObserverActivity : AppCompatActivity() {
-    protected inline fun <T> LiveData<T>.observe(crossinline block: (T?) -> Unit) {
+    protected inline fun <T> LiveData<T>.observe(crossinline block: (T) -> Unit) {
         observe(this@LiveDataObserverActivity, Observer { block(it) })
     }
 
-    protected inline fun <T> LiveData<T>.observeNotNull(crossinline block: (T) -> Unit) {
+    protected inline fun <T> LiveData<T?>.observeNotNull(crossinline block: (T) -> Unit) {
         observe(this@LiveDataObserverActivity, Observer {
             it ?: return@Observer
             block(it)
--- app/src/main/kotlin/org/jdc/template/ui/fragment/LiveDataObserverFragment.kt
+++ app/src/main/kotlin/org/jdc/template/ui/fragment/LiveDataObserverFragment.kt
@@ -5,11 +5,11 @@ import androidx.lifecycle.Observer
 import androidx.fragment.app.Fragment
 
 abstract class LiveDataObserverFragment : Fragment() {
-    protected inline fun <T> LiveData<T>.observe(crossinline block: (T?) -> Unit) {
+    protected inline fun <T> LiveData<T>.observe(crossinline block: (T) -> Unit) {
         observe(this@LiveDataObserverFragment, Observer { block(it) })
     }
 
-    protected inline fun <T> LiveData<T>.observeNotNull(crossinline block: (T) -> Unit) {
+    protected inline fun <T> LiveData<T?>.observeNotNull(crossinline block: (T) -> Unit) {
         observe(this@LiveDataObserverFragment, Observer {
             it ?: return@Observer
             block(it)
