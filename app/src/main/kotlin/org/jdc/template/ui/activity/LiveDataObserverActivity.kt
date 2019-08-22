0bfb56afb0837f9ec37fe0e32c1a18ef92360eb1
jeffdcamp <jeffdcamp@gmail.com>
2019-08-22 15:12:41 -0600

    Improved Activity/Fragment observe methods

--- app/src/main/kotlin/org/jdc/template/ui/activity/LiveDataObserverActivity.kt
+++ app/src/main/kotlin/org/jdc/template/ui/activity/LiveDataObserverActivity.kt
@@ -1,18 +1,26 @@
 package org.jdc.template.ui.activity
 
+import androidx.annotation.MainThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class LiveDataObserverActivity : AppCompatActivity() {
-    protected inline fun <T> LiveData<T>.observe(crossinline block: (T) -> Unit) {
-        observe(this@LiveDataObserverActivity, Observer { block(it) })
+    @MainThread
+    protected inline fun <T> LiveData<T>.observe(crossinline onChanged: (T?) -> Unit): Observer<T> {
+        val wrappedObserver = Observer<T> { value -> onChanged.invoke(value) }
+        observe(this@LiveDataObserverActivity, wrappedObserver)
+        return wrappedObserver
     }
 
-    protected inline fun <T> LiveData<T?>.observeNotNull(crossinline block: (T) -> Unit) {
-        observe(this@LiveDataObserverActivity, Observer {
-            it ?: return@Observer
-            block(it)
-        })
+    @MainThread
+    protected inline fun <T> LiveData<T?>.observeNotNull(crossinline onChanged: (T) -> Unit): Observer<T?> {
+        val wrappedObserver = Observer<T?> { value ->
+            if (value != null) {
+                onChanged.invoke(value)
+            }
+        }
+        observe(this@LiveDataObserverActivity, wrappedObserver)
+        return wrappedObserver
     }
 }
--- app/src/main/kotlin/org/jdc/template/ui/fragment/LiveDataObserverFragment.kt
+++ app/src/main/kotlin/org/jdc/template/ui/fragment/LiveDataObserverFragment.kt
@@ -1,18 +1,46 @@
 package org.jdc.template.ui.fragment
 
+import androidx.annotation.MainThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.fragment.app.Fragment
 
 abstract class LiveDataObserverFragment : Fragment() {
-    protected inline fun <T> LiveData<T>.observe(crossinline block: (T) -> Unit) {
-        observe(this@LiveDataObserverFragment, Observer { block(it) })
+    @MainThread
+    protected inline fun <T> LiveData<T>.observe(crossinline onChanged: (T?) -> Unit): Observer<T> {
+        val wrappedObserver = Observer<T> { value ->
+            onChanged.invoke(value)
+        }
+        observe(viewLifecycleOwner, wrappedObserver)
+        return wrappedObserver
     }
 
-    protected inline fun <T> LiveData<T?>.observeNotNull(crossinline block: (T) -> Unit) {
-        observe(this@LiveDataObserverFragment, Observer {
-            it ?: return@Observer
-            block(it)
-        })
+    @MainThread
+    protected inline fun <T> LiveData<T?>.observeNotNull(crossinline onChanged: (T) -> Unit): Observer<T?> {
+        val wrappedObserver = Observer<T?> { value ->
+            if (value != null) {
+                onChanged.invoke(value)
+            }
+        }
+        observe(viewLifecycleOwner, wrappedObserver)
+        return wrappedObserver
+    }
+
+    @MainThread
+    protected inline fun <T> LiveData<T>.observeByFragment(crossinline onChanged: (T?) -> Unit): Observer<T> {
+        val wrappedObserver = Observer<T> { value -> onChanged.invoke(value) }
+        observe(this@LiveDataObserverFragment, wrappedObserver)
+        return wrappedObserver
+    }
+
+    @MainThread
+    protected inline fun <T> LiveData<T?>.observeNotNullByFragment(crossinline onChanged: (T) -> Unit): Observer<T?> {
+        val wrappedObserver = Observer<T?> { value ->
+            if (value != null) {
+                onChanged.invoke(value)
+            }
+        }
+        observe(this@LiveDataObserverFragment, wrappedObserver)
+        return wrappedObserver
     }
 }
