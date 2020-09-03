3c839798bb52d91f14d92b489287508c4db854f1
Troy Heninger <troyhen@churchofjesuschrist.org>
2020-09-02 18:11:55 -0600

    Simplify Fragment setContent extension with ComposeView

--- app/src/main/kotlin/org/jdc/template/ui/compose/Fragment.kt
+++ app/src/main/kotlin/org/jdc/template/ui/compose/Fragment.kt
@@ -2,15 +2,11 @@ package org.jdc.template.ui.compose
 
 import android.view.View
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Recomposer
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.setContent
 import androidx.fragment.app.Fragment
 
 fun Fragment.setContent(content: @Composable () -> Unit): View {
     return ComposeView(requireContext()).apply {
-        setContent(Recomposer.current()) {
-            content()
-        }
+        setContent(content)
     }
 }
