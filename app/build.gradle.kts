7609c5d36d340d035a54b12a19340f68d0059947
jeffdcamp <jeffdcamp@gmail.com>
2021-06-04 17:05:22 -0600

    WorkManager 2.6.0-beta01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -210,7 +210,7 @@ dependencies {
     // WorkManager
     implementation(libs.androidx.work.runtime)
     implementation(libs.androidx.work.gcm)
-    implementation(libs.workmanagertools)
+//    implementation(libs.workmanagertools)
 
     // Database
     implementation(libs.androidx.room.runtime)
