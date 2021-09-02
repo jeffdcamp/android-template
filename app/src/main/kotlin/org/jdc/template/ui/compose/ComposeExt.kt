9fc0d1d8c7de81808b93bc76f6df370ea8216176
jeffdcamp <jeffdcamp@gmail.com>
2021-09-02 17:40:05 -0600

    Added comments to LocalNavController

--- app/src/main/kotlin/org/jdc/template/ui/compose/ComposeExt.kt
+++ app/src/main/kotlin/org/jdc/template/ui/compose/ComposeExt.kt
@@ -13,6 +13,16 @@ import kotlinx.coroutines.flow.Flow
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
+/**
+ * Used to grant access to the Nav Controller inside compose functions
+ *
+ * This must be set in the fragment or where the nav controller is created as follows
+ *
+ * CompositionLocalProvider(LocalNavController provides navController) {
+ *     // Rest of compose code
+ *     // LocalNavController.value will be present.
+ * }
+ */
 val LocalNavController = staticCompositionLocalOf<NavController?> { null }
 
 /**
