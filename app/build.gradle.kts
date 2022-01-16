352928d4d358d56bf936f759fe8f63f330b970fd
jeffdcamp <jeffdcamp@gmail.com>
2022-01-16 11:35:24 -0700

    Updated versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+        kotlinCompilerExtensionVersion = libs.versions.compose.get()
     }
 
     lint {
