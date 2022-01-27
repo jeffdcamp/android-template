94dbfe4b28212fa6422ed632a3b43d60b2ca3759
jeffdcamp <jeffdcamp@gmail.com>
2022-01-26 21:04:27 -0700

    Improvements to InputDialog.kt to better support TextFieldValue

--- app/src/main/kotlin/org/jdc/template/ui/compose/dialog/InputDialog.kt
+++ app/src/main/kotlin/org/jdc/template/ui/compose/dialog/InputDialog.kt
@@ -1,5 +1,4 @@
 package org.jdc.template.ui.compose.dialog
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -18,6 +17,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -39,11 +40,10 @@ fun InputDialog(
     maxLength: Int = -1,
     properties: DialogProperties = DialogProperties(),
     shape: Shape = MaterialTheme.shapes.medium,
-    backgroundColor: Color = MaterialTheme.colors.surface
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    textButtonColor: Color = MaterialTheme.colors.primary, // This is specifically for handling theming in Gospel Library. May not want in Commons.
 ) {
-    var textValue: String by rememberSaveable {
-        mutableStateOf(initialTextFieldText ?: "")
-    }
+    var textFieldValue by rememberSaveable(stateSaver = TextFieldValue.Saver) { mutableStateOf(TextFieldValue(initialTextFieldText ?: "", TextRange(initialTextFieldText?.length ?: 0))) }
 
     // Test: request focus on TextField
 //    val focusRequester = remember { FocusRequester() }
@@ -69,13 +69,15 @@ fun InputDialog(
 
                 // Text Field
                 DayNightTextField(
-                    value = textValue,
-                    onValueChange = {
-                        textValue = if (maxLength > 0) {
-                            it.take(maxLength)
+                    value = textFieldValue,
+                    onValueChange = { newTextFieldValue ->
+                        val newText = if (maxLength > 0) {
+                            newTextFieldValue.text.take(maxLength)
                         } else {
-                            it
+                            newTextFieldValue.text
                         }
+
+                        textFieldValue = newTextFieldValue.copy(newText, selection = TextRange(newText.length))
                     },
                     label = if (textFieldLabel != null) {
                         { Text(textFieldLabel) }
@@ -101,17 +103,143 @@ fun InputDialog(
                                 onDismissButtonClicked()
                             }
                         ) {
-                            Text(dismissButtonText.toUpperCase(Locale.getDefault()))
+                            Text(dismissButtonText.uppercase(Locale.getDefault()), color = textButtonColor)
                         }
                     }
                     if (onConfirmButtonClicked != null) {
                         TextButton(
-                            enabled = minLength == -1 || textValue.length >= minLength,
+                            enabled = minLength == -1 || textFieldValue.text.length >= minLength,
                             onClick = {
-                                onConfirmButtonClicked(textValue)
+                                onConfirmButtonClicked(textFieldValue.text)
                             }
                         ) {
-                            Text(confirmButtonText.toUpperCase(Locale.getDefault()))
+                            Text(confirmButtonText.uppercase(Locale.getDefault()), color = textButtonColor)
+                        }
+                    }
+                }
+
+                // Delay requesting focus on TextField so it does not happen during composition
+//                DisposableEffect(Unit) {
+//                    focusRequester.requestFocus()
+//                    onDispose { }
+//                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TwoInputDialog(
+    onDismissRequest: (() -> Unit) = {},
+    title: String? = null,
+    textFieldLabelFirst: String? = null,
+    initialTextFieldTextFirst: String? = null,
+    textFieldLabelSecond: String? = null,
+    initialTextFieldTextSecond: String? = null,
+    confirmButtonText: String = stringResource(R.string.ok),
+    onConfirmButtonClicked: ((Pair<String, String>) -> Unit)? = null,
+    dismissButtonText: String = stringResource(R.string.cancel),
+    onDismissButtonClicked: (() -> Unit)? = null,
+    minLengthFirst: Int = -1,
+    maxLengthFirst: Int = -1,
+    minLengthSecond: Int = -1,
+    maxLengthSecond: Int = -1,
+    properties: DialogProperties = DialogProperties(),
+    shape: Shape = MaterialTheme.shapes.medium,
+    backgroundColor: Color = MaterialTheme.colors.surface,
+    textButtonColor: Color = MaterialTheme.colors.primary, // This is specifically for handling theming in Gospel Library. May not want in Commons.
+) {
+    var textFieldValueFirst by rememberSaveable(stateSaver = TextFieldValue.Saver) { mutableStateOf(TextFieldValue(initialTextFieldTextFirst ?: "", TextRange(initialTextFieldTextFirst?.length ?: 0))) }
+    var textFieldValueSecond by rememberSaveable(stateSaver = TextFieldValue.Saver) { mutableStateOf(TextFieldValue(initialTextFieldTextSecond ?: "", TextRange(initialTextFieldTextSecond?.length ?: 0))) }
+
+    // Test: request focus on TextField
+//    val focusRequester = remember { FocusRequester() }
+//    val keyboardController = LocalSoftwareKeyboardController.current // keyboardController?.show()
+
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = properties,
+    ) {
+        Surface(
+            shape = shape,
+            color = backgroundColor
+        ) {
+            Column {
+                // Title
+                if (title != null) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.h6,
+                        modifier = Modifier.padding(top = 16.dp, start = 16.dp)
+                    )
+                }
+
+                // Text Fields
+                DayNightTextField(
+                    value = textFieldValueFirst,
+                    onValueChange = { newTextFieldValue ->
+                        val newText = if (maxLengthFirst > 0) {
+                            newTextFieldValue.text.take(maxLengthFirst)
+                        } else {
+                            newTextFieldValue.text
+                        }
+
+                        textFieldValueFirst = newTextFieldValue.copy(newText, selection = TextRange(newText.length))
+                    },
+                    label = if (textFieldLabelFirst != null) {
+                        { Text(textFieldLabelFirst) }
+                    } else null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+//                        .focusRequester(focusRequester)
+                )
+                DayNightTextField(
+                    value = textFieldValueSecond,
+                    onValueChange = { newTextFieldValue ->
+                        val newText = if (maxLengthSecond > 0) {
+                            newTextFieldValue.text.take(maxLengthSecond)
+                        } else {
+                            newTextFieldValue.text
+                        }
+
+                        textFieldValueSecond = newTextFieldValue.copy(newText, selection = TextRange(newText.length))
+                    },
+                    label = if (textFieldLabelSecond != null) {
+                        { Text(textFieldLabelSecond) }
+                    } else null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+//                        .focusRequester(focusRequester)
+                )
+
+
+                // Buttons
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(all = 8.dp)
+
+                ) {
+                    if (onDismissButtonClicked != null) {
+                        TextButton(
+                            onClick = {
+                                onDismissButtonClicked()
+                            }
+                        ) {
+                            Text(dismissButtonText.uppercase(Locale.getDefault()), color = textButtonColor)
+                        }
+                    }
+                    if (onConfirmButtonClicked != null) {
+                        TextButton(
+                            enabled = (minLengthFirst == -1 || textFieldValueFirst.text.length >= minLengthFirst) && (minLengthSecond == -1 || textFieldValueSecond.text.length >= minLengthSecond),
+                            onClick = {
+                                onConfirmButtonClicked(Pair(textFieldValueFirst.text, textFieldValueSecond.text))
+                            }
+                        ) {
+                            Text(confirmButtonText.uppercase(Locale.getDefault()), color = textButtonColor)
                         }
                     }
                 }
@@ -132,4 +260,9 @@ data class InputDialogData(
     val initialTextFieldText: String? = null
 )
 
-
+data class TwoInputDialogData(
+    val visible: Boolean = false,
+    val title: String? = null,
+    val initialTextFieldTextFirst: String? = null,
+    val initialTextFieldTextSecond: String? = null
+)
