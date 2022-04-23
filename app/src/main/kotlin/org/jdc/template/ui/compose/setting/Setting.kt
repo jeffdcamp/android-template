12c78f6b165b22a1715679470c594cb6c1dfa82e
jeffdcamp <jeffdcamp@gmail.com>
2022-04-23 13:52:09 -0600

    Added Preview to Setting.kt

--- app/src/main/kotlin/org/jdc/template/ui/compose/setting/Setting.kt
+++ app/src/main/kotlin/org/jdc/template/ui/compose/setting/Setting.kt
@@ -1,17 +1,25 @@
 package org.jdc.template.ui.compose.setting
 
+import android.content.res.Configuration
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import org.jdc.template.ui.theme.AppTheme
 
 object Setting {
     @Composable
@@ -117,5 +125,33 @@ object Setting {
             icon = icon,
             onClickBody = onClickBody
         )
+    }
+}
+
+@Preview(group = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL, showBackground = true)
+@Preview(group = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL, showBackground = true)
+@Composable
+private fun SettingsPreview() {
+    val currentThemeTitleFlow = MutableStateFlow("Light Theme")
+    val sortByLastNameFlow = MutableStateFlow(true)
+    val currentLastInstalledVersionCodeFlow = MutableStateFlow("1234")
+
+    AppTheme {
+        Scaffold {
+            val scrollState = rememberScrollState()
+
+            Column(
+                Modifier.verticalScroll(scrollState)
+            ) {
+                Setting.Header("Display")
+                Setting.Clickable("Theme", currentThemeTitleFlow) { }
+                Setting.Switch("Sort by last name", sortByLastNameFlow) { }
+
+                // not translated because this should not be visible for release builds
+                Setting.Header("Developer Options")
+                Setting.Clickable(text = "Work Manager Status", secondaryText = "Show status of all background workers") { }
+                Setting.Clickable(text = "Last Installed Version Code", currentLastInstalledVersionCodeFlow) { }
+            }
+        }
     }
 }
