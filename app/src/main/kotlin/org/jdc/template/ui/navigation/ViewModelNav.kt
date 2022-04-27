2fb965924f740c671833bea854e63ab195a619ca
jeffdcamp <jeffdcamp@gmail.com>
2022-04-26 18:41:21 -0600

    Added support for ViewModelNav to navigate to an Intent

--- app/src/main/kotlin/org/jdc/template/ui/navigation/ViewModelNav.kt
+++ app/src/main/kotlin/org/jdc/template/ui/navigation/ViewModelNav.kt
@@ -1,9 +1,13 @@
 package org.jdc.template.ui.navigation
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import androidx.navigation.NavOptionsBuilder
@@ -22,6 +26,7 @@ interface ViewModelNav {
     fun navigate(routes: List<String>)
     fun navigate(route: String, navOptions: NavOptions)
     fun navigate(route: String, optionsBuilder: NavOptionsBuilder.() -> Unit)
+    fun navigate(intent: Intent, options: Bundle? = null)
     fun popBackStack(popToRoute: String? = null, inclusive: Boolean = false)
     fun navigate(viewModelNavigator: ViewModelNavigator)
     fun resetNavigate(viewModelNavigator: ViewModelNavigator)
@@ -47,6 +52,10 @@ class ViewModelNavImpl : ViewModelNav {
         _navigatorFlow.compareAndSet(null, ViewModelNavigator.NavigateWithOptions(route, navOptions(optionsBuilder)))
     }
 
+    override fun navigate(intent: Intent, options: Bundle?) {
+        _navigatorFlow.compareAndSet(null, ViewModelNavigator.NavigateIntent(intent, options))
+    }
+
     override fun popBackStack(popToRoute: String?, inclusive: Boolean) {
         _navigatorFlow.compareAndSet(null, ViewModelNavigator.Pop(popToRoute, inclusive))
     }
@@ -64,10 +73,10 @@ sealed class ViewModelNavigator {
     /**
      * @return true if navController.popBackStack() called AND was successful
      */
-    abstract fun navigate(navController: NavController, viewModelNav: ViewModelNav): Boolean
+    abstract fun navigate(context: Context, navController: NavController, viewModelNav: ViewModelNav): Boolean
 
     class Navigate(private val route: String) : ViewModelNavigator() {
-        override fun navigate(navController: NavController, viewModelNav: ViewModelNav): Boolean {
+        override fun navigate(context: Context, navController: NavController, viewModelNav: ViewModelNav): Boolean {
             navController.navigate(route)
 
             viewModelNav.resetNavigate(this)
@@ -76,7 +85,7 @@ sealed class ViewModelNavigator {
     }
 
     class NavigateMultiple(private val routes: List<String>) : ViewModelNavigator() {
-        override fun navigate(navController: NavController, viewModelNav: ViewModelNav): Boolean {
+        override fun navigate(context: Context, navController: NavController, viewModelNav: ViewModelNav): Boolean {
             routes.forEach { route ->
                 navController.navigate(route)
             }
@@ -87,7 +96,7 @@ sealed class ViewModelNavigator {
     }
 
     class NavigateWithOptions(private val route: String, private val navOptions: NavOptions) : ViewModelNavigator() {
-        override fun navigate(navController: NavController, viewModelNav: ViewModelNav): Boolean {
+        override fun navigate(context: Context, navController: NavController, viewModelNav: ViewModelNav): Boolean {
             navController.navigate(route, navOptions)
 
             viewModelNav.resetNavigate(this)
@@ -95,8 +104,16 @@ sealed class ViewModelNavigator {
         }
     }
 
+    class NavigateIntent(val intent: Intent, val options: Bundle? = null) : ViewModelNavigator() {
+        override fun navigate(context: Context, navController: NavController, viewModelNav: ViewModelNav): Boolean {
+            context.startActivity(intent, options)
+            viewModelNav.resetNavigate(this)
+            return false
+        }
+    }
+
     class PopAndNavigate(private val route: String) : ViewModelNavigator() {
-        override fun navigate(navController: NavController, viewModelNav: ViewModelNav): Boolean {
+        override fun navigate(context: Context, navController: NavController, viewModelNav: ViewModelNav): Boolean {
             val stackPopped = navController.popBackStack()
             navController.navigate(route)
 
@@ -106,7 +123,7 @@ sealed class ViewModelNavigator {
     }
 
     class Pop(private val popToRoute: String?, private val inclusive: Boolean = false) : ViewModelNavigator() {
-        override fun navigate(navController: NavController, viewModelNav: ViewModelNav): Boolean {
+        override fun navigate(context: Context, navController: NavController, viewModelNav: ViewModelNav): Boolean {
             val stackPopped = if (popToRoute == null) {
                 navController.popBackStack()
             } else {
@@ -126,7 +143,9 @@ fun HandleNavigation(
 ) {
     navController ?: return
     val navigator by viewModelNav.navigatorFlow.collectAsState()
+
+    val context = LocalContext.current
     LaunchedEffect(navigator) {
-        navigator?.navigate(navController, viewModelNav)
+        navigator?.navigate(context, navController, viewModelNav)
     }
 }
