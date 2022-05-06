18591c3f66521c86521d73aa1f6e99f062b3794a
jeffdcamp <jeffdcamp@gmail.com>
2022-05-06 09:06:19 -0600

    Added EnumExposedDropdownMenuBox

---
+++ app/src/main/kotlin/org/jdc/template/ui/compose/EnumExposedDropdownMenuBox.kt
@@ -0,0 +1,58 @@
+package org.jdc.template.ui.compose
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.flow.StateFlow
+
+@Composable
+fun <T: Enum<T>> EnumExposedDropdownMenuBox(
+    options: List<T>,
+    selectedOptionFlow: StateFlow<T>,
+    onOptionSelected: (T) -> Unit,
+    optionToText: @Composable (T) -> String,
+    modifier: Modifier = Modifier,
+    label: String? = null
+) {
+    val selectedOption by selectedOptionFlow.collectAsState()
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = modifier
+    ) {
+        DayNightTextField(
+            readOnly = true,
+            value = optionToText(selectedOption),
+            onValueChange = {},
+            label = if (label != null) { { Text(text = label) } } else null,
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(
+                    text = { Text(optionToText(option)) },
+                    onClick = {
+                        onOptionSelected(option)
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
