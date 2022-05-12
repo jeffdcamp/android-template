3c85958108169e88d816da35584949f3556f165d
jeffdcamp <jeffdcamp@gmail.com>
2022-05-11 19:03:55 -0600

    Fixed detekt issue

--- app/src/main/kotlin/org/jdc/template/ui/compose/Previews.kt
+++ app/src/main/kotlin/org/jdc/template/ui/compose/Previews.kt
@@ -5,17 +5,17 @@ import androidx.compose.ui.tooling.preview.Preview
 
 @PreviewLightDarkDevices
 @PreviewMultiSizeDevices
-annotation class PreviewAll()
+annotation class PreviewAll
 
 @Preview(group = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL, showBackground = true)
 @Preview(group = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL, showBackground = true)
-annotation class PreviewLightDarkDevices()
+annotation class PreviewLightDarkDevices
 
 @Preview(group = "Phone", device = "spec:shape=Normal,width=640,height=360,unit=dp,dpi=480", name = "Phone Landscape", showBackground = true)
 @Preview(group = "Phone", device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480", name = "Phone Portrait", showBackground = true)
 @Preview(group = "Tablet", device = "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480", name = "Tablet Landscape", showBackground = true)
 @Preview(group = "Tablet", device = "spec:shape=Normal,width=800,height=1280,unit=dp,dpi=480", name = "Tablet Portrait", showBackground = true)
-annotation class PreviewMultiSizeDevices()
+annotation class PreviewMultiSizeDevices
 
 
 
