61abea66b960307fb1bc6e6091126fcd5ea64e96
jeffdcamp <jeffdcamp@gmail.com>
2022-06-01 22:14:15 -0600

    Fixed IndividualEditScreenTest.kt

--- app/src/androidTest/java/org/jdc/template/ux/individualedit/IndividualEditScreenTest.kt
+++ app/src/androidTest/java/org/jdc/template/ux/individualedit/IndividualEditScreenTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertTextContains
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import kotlinx.coroutines.flow.MutableStateFlow
+import org.jdc.template.ui.compose.TextFieldData
 import org.jdc.template.ui.theme.AppTheme
 import org.junit.Rule
 import org.junit.Test
@@ -14,20 +15,20 @@ class IndividualEditScreenTest {
 
     @Test
     fun testFields() {
-        val firstNameFlow = MutableStateFlow(TextFieldValue("Jeff"))
-        val lastNameFlow = MutableStateFlow(TextFieldValue("Campbell"))
-        val phoneNumberFlow = MutableStateFlow(TextFieldValue("801-555-0000"))
-        val emailFlow = MutableStateFlow(TextFieldValue(""))
+        val firstNameFlow = MutableStateFlow(TextFieldData("Jeff"))
+        val lastNameFlow = MutableStateFlow(TextFieldData("Campbell"))
+        val phoneNumberFlow = MutableStateFlow(TextFieldData("801-555-0000"))
+        val emailFlow = MutableStateFlow(TextFieldData(""))
 
         val individualEditUiState = IndividualEditUiState(
             firstNameFlow = firstNameFlow,
-            firstNameOnChange = { firstNameFlow.value = TextFieldValue(it) },
+            firstNameOnChange = { firstNameFlow.value = TextFieldData(it) },
             lastNameFlow = lastNameFlow,
-            lastNameOnChange = { lastNameFlow.value = TextFieldValue(it) },
+            lastNameOnChange = { lastNameFlow.value = TextFieldData(it) },
             phoneFlow = phoneNumberFlow,
-            phoneOnChange = { phoneNumberFlow.value = TextFieldValue(it) },
+            phoneOnChange = { phoneNumberFlow.value = TextFieldData(it) },
             emailFlow = emailFlow,
-            emailOnChange = { emailFlow.value = TextFieldValue(it) }
+            emailOnChange = { emailFlow.value = TextFieldData(it) }
         )
 
         composeTestRule.setContent {
