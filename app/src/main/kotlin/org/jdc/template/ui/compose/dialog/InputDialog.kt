6c485e07019e374e261ea32ac9d4afe5175921fb
jeffdcamp <jeffdcamp@gmail.com>
2022-06-01 22:14:53 -0600

    SDK API 32 / Compose 1.2.0-beta03 / Material3 alpha13

--- app/src/main/kotlin/org/jdc/template/ui/compose/dialog/InputDialog.kt
+++ app/src/main/kotlin/org/jdc/template/ui/compose/dialog/InputDialog.kt
@@ -1,6 +1,5 @@
 package org.jdc.template.ui.compose.dialog
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -30,7 +29,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
--- buildSrc/src/main/kotlin/AppInfo.kt
+++ buildSrc/src/main/kotlin/AppInfo.kt
@@ -13,7 +13,7 @@ object AppInfo {
 
     object AndroidSdk {
         const val MIN = 21
-        const val COMPILE = 31
-        const val TARGET = 31
+        const val COMPILE = 32
+        const val TARGET = 32
     }
 }
