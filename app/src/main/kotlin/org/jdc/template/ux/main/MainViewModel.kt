aea3cee797f797823476259996263ec2ac33fe37
jeffdcamp <jeffdcamp@gmail.com>
2022-10-27 12:23:44 -0600

    Simplified creation of NavBarConfig

--- app/src/main/kotlin/org/jdc/template/ux/main/MainViewModel.kt
+++ app/src/main/kotlin/org/jdc/template/ux/main/MainViewModel.kt
@@ -7,12 +7,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import org.jdc.template.domain.individual.CreateIndividualTestDataUseCase
 import org.jdc.template.model.repository.SettingsRepository
 import org.jdc.template.ui.navigation.DefaultNavBarConfig
-import org.jdc.template.ui.navigation.NavBarConfig
 import org.jdc.template.ui.navigation.ViewModelNavBar
 import org.jdc.template.ui.navigation.ViewModelNavBarImpl
 import org.jdc.template.util.ext.stateInDefault
-import org.jdc.template.ux.about.AboutRoute
-import org.jdc.template.ux.directory.DirectoryRoute
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,7 +17,7 @@ class MainViewModel
 @Inject constructor(
     settingsRepository: SettingsRepository,
     private val createIndividualTestDataUseCase: CreateIndividualTestDataUseCase
-) : ViewModel(), ViewModelNavBar<NavBarItem> by ViewModelNavBarImpl(NavBarItem.PEOPLE, createNavBarConfig()) {
+) : ViewModel(), ViewModelNavBar<NavBarItem> by ViewModelNavBarImpl(NavBarItem.PEOPLE, DefaultNavBarConfig(NavBarItem.getNavBarItemRouteMap())) {
     val uiState = MainUiState(
         themeFlow = settingsRepository.themeFlow.stateInDefault(viewModelScope, null)
     )
@@ -28,16 +25,5 @@ class MainViewModel
     @VisibleForTesting
     suspend fun createSampleData() {
         createIndividualTestDataUseCase()
-    }
-
-    companion object {
-        fun createNavBarConfig(): NavBarConfig<NavBarItem>  {
-            return DefaultNavBarConfig(
-                mapOf(
-                    NavBarItem.PEOPLE to DirectoryRoute.createRoute(),
-                    NavBarItem.ABOUT to AboutRoute.createRoute()
-                )
-            )
-        }
     }
 }
--- app/src/main/kotlin/org/jdc/template/ux/main/NavBarItem.kt
+++ app/src/main/kotlin/org/jdc/template/ux/main/NavBarItem.kt
@@ -6,11 +6,20 @@ import androidx.compose.material.icons.outlined.Info
 import androidx.compose.ui.graphics.vector.ImageVector
 import org.jdc.template.R
 import org.jdc.template.ui.compose.icons.google.outlined.People
+import org.jdc.template.ux.about.AboutRoute
+import org.jdc.template.ux.directory.DirectoryRoute
 
 enum class NavBarItem(
     val imageVector: ImageVector,
+    val route: String,
     @StringRes val textResId: Int? = null,
 ) {
-    PEOPLE(Icons.Outlined.People, R.string.people),
-    ABOUT(Icons.Outlined.Info, R.string.about);
+    PEOPLE(Icons.Outlined.People, DirectoryRoute.createRoute(), R.string.people),
+    ABOUT(Icons.Outlined.Info, AboutRoute.createRoute(), R.string.about);
+
+    companion object {
+        fun getNavBarItemRouteMap(): Map<NavBarItem, String> {
+            return values().associateWith { item -> item.route }
+        }
+    }
 }
