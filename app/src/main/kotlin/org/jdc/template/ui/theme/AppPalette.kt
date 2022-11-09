77430dbf6061c42331cf70124de446b97f8448b5
jeffdcamp <jeffdcamp@gmail.com>
2022-11-09 13:34:14 -0700

    Updated AppPalette

--- app/src/main/kotlin/org/jdc/template/ui/theme/AppPalette.kt
+++ app/src/main/kotlin/org/jdc/template/ui/theme/AppPalette.kt
@@ -4,22 +4,23 @@ import androidx.compose.ui.graphics.Color
 
 // Colors based on the following colors -> https://m3.material.io/theme-builder#/custom
 // Primary ba1a20
-// Secondary 005fb1
-// Tertiary 00696b
+// Secondary 680008
+// Tertiary a88d5b
+// Neutral 998e8d
 
 object AppPalette {
     val md_theme_light_primary = Color(0xFFBA1A20)
     val md_theme_light_onPrimary = Color(0xFFFFFFFF)
     val md_theme_light_primaryContainer = Color(0xFFFFDAD6)
     val md_theme_light_onPrimaryContainer = Color(0xFF410003)
-    val md_theme_light_secondary = Color(0xFF005FB0)
+    val md_theme_light_secondary = Color(0xFFAA3430)
     val md_theme_light_onSecondary = Color(0xFFFFFFFF)
-    val md_theme_light_secondaryContainer = Color(0xFFD5E3FF)
-    val md_theme_light_onSecondaryContainer = Color(0xFF001C3B)
-    val md_theme_light_tertiary = Color(0xFF00696B)
+    val md_theme_light_secondaryContainer = Color(0xFFFFDAD6)
+    val md_theme_light_onSecondaryContainer = Color(0xFF410003)
+    val md_theme_light_tertiary = Color(0xFF725B2E)
     val md_theme_light_onTertiary = Color(0xFFFFFFFF)
-    val md_theme_light_tertiaryContainer = Color(0xFF6FF6F9)
-    val md_theme_light_onTertiaryContainer = Color(0xFF002021)
+    val md_theme_light_tertiaryContainer = Color(0xFFFEDEA6)
+    val md_theme_light_onTertiaryContainer = Color(0xFF261900)
     val md_theme_light_error = Color(0xFFBA1A1A)
     val md_theme_light_errorContainer = Color(0xFFFFDAD6)
     val md_theme_light_onError = Color(0xFFFFFFFF)
@@ -36,19 +37,21 @@ object AppPalette {
     val md_theme_light_inversePrimary = Color(0xFFFFB3AC)
     val md_theme_light_shadow = Color(0xFF000000)
     val md_theme_light_surfaceTint = Color(0xFFBA1A20)
+    val md_theme_light_outlineVariant = Color(0xFFD8C2BF)
+    val md_theme_light_scrim = Color(0xFF000000)
 
     val md_theme_dark_primary = Color(0xFFFFB3AC)
     val md_theme_dark_onPrimary = Color(0xFF680008)
     val md_theme_dark_primaryContainer = Color(0xFF930010)
     val md_theme_dark_onPrimaryContainer = Color(0xFFFFDAD6)
-    val md_theme_dark_secondary = Color(0xFFA6C8FF)
-    val md_theme_dark_onSecondary = Color(0xFF00315F)
-    val md_theme_dark_secondaryContainer = Color(0xFF004787)
-    val md_theme_dark_onSecondaryContainer = Color(0xFFD5E3FF)
-    val md_theme_dark_tertiary = Color(0xFF4CDADD)
-    val md_theme_dark_onTertiary = Color(0xFF003738)
-    val md_theme_dark_tertiaryContainer = Color(0xFF004F51)
-    val md_theme_dark_onTertiaryContainer = Color(0xFF6FF6F9)
+    val md_theme_dark_secondary = Color(0xFFFFB3AC)
+    val md_theme_dark_onSecondary = Color(0xFF680008)
+    val md_theme_dark_secondaryContainer = Color(0xFF891C1C)
+    val md_theme_dark_onSecondaryContainer = Color(0xFFFFDAD6)
+    val md_theme_dark_tertiary = Color(0xFFE1C38C)
+    val md_theme_dark_onTertiary = Color(0xFF402D04)
+    val md_theme_dark_tertiaryContainer = Color(0xFF584419)
+    val md_theme_dark_onTertiaryContainer = Color(0xFFFEDEA6)
     val md_theme_dark_error = Color(0xFFFFB4AB)
     val md_theme_dark_errorContainer = Color(0xFF93000A)
     val md_theme_dark_onError = Color(0xFF690005)
@@ -65,18 +68,9 @@ object AppPalette {
     val md_theme_dark_inversePrimary = Color(0xFFBA1A20)
     val md_theme_dark_shadow = Color(0xFF000000)
     val md_theme_dark_surfaceTint = Color(0xFFFFB3AC)
-
-    val seed = Color(0xFFBA1A20)
-
-    // extra colors not yet provided by tool
-    // light
-    val md_theme_light_outlineVariant = Color(0xFF79747E)
-    val md_theme_light_scrim = Color(0xFF000000)
-
-    // dark
-    val md_theme_dark_outlineVariant = Color(0xFF938F99)
+    val md_theme_dark_outlineVariant = Color(0xFF534342)
     val md_theme_dark_scrim = Color(0xFF000000)
 
-    // both
-    val error = Color(0xFFB3261E)
+
+    val seed = Color(0xFFBA1A20)
 }
