8d545d46f06eb8232a9d594aadf7b6ddd0022057
jeffdcamp <jeffdcamp@gmail.com>
2023-05-01 10:09:34 -0600

    Expanded SavedStateHandleExt.kt

--- app/src/main/kotlin/org/jdc/template/util/ext/SavedStateHandleExt.kt
+++ app/src/main/kotlin/org/jdc/template/util/ext/SavedStateHandleExt.kt
@@ -7,16 +7,22 @@ import java.net.URLDecoder
 @VisibleForTesting
 fun createSaveStateErrorMessage(key: String) = "Missing SavedState value for Key: $key"
 
+fun SavedStateHandle.requireBoolean(key: String): Boolean = requireNotNull(get<Boolean>(key)) { createSaveStateErrorMessage(key) }
 fun SavedStateHandle.requireInt(key: String): Int = requireNotNull(get<Int>(key)) { createSaveStateErrorMessage(key) }
+fun SavedStateHandle.requireLong(key: String): Long = requireNotNull(get<Long>(key)) { createSaveStateErrorMessage(key) }
 fun SavedStateHandle.requireString(key: String): String = requireNotNull(get<String>(key)) { createSaveStateErrorMessage(key) }
 
-fun SavedStateHandle.decodedString(key: String): String? = get<String>(key)?.let { URLDecoder.decode(it, "utf-8") }
+inline fun <T> SavedStateHandle.getValueClassBoolean(key: String, mapToValueClass: (Int) -> T?): T? = getValueClass<Int, T>(key, mapToValueClass)
+inline fun <T> SavedStateHandle.requireValueClassBoolean(key: String, mapToValueClass: (Int) -> T?): T = requireValueClass<Int, T>(key, mapToValueClass)
+inline fun <T> SavedStateHandle.getValueClassInt(key: String, mapToValueClass: (Int) -> T?): T? = getValueClass<Int, T>(key, mapToValueClass)
+inline fun <T> SavedStateHandle.requireValueClassInt(key: String, mapToValueClass: (Int) -> T?): T = requireValueClass<Int, T>(key, mapToValueClass)
+inline fun <T> SavedStateHandle.getValueClassLong(key: String, mapToValueClass: (Int) -> T?): T? = getValueClass<Int, T>(key, mapToValueClass)
+inline fun <T> SavedStateHandle.requireValueClassLong(key: String, mapToValueClass: (Int) -> T?): T = requireValueClass<Int, T>(key, mapToValueClass)
+inline fun <T> SavedStateHandle.getValueClassString(key: String, mapToValueClass: (String) -> T?): T? = getValueClass<String, T>(key, mapToValueClass)
+inline fun <T> SavedStateHandle.requireValueClassString(key: String, mapToValueClass: (String) -> T?): T = requireValueClass<String, T>(key, mapToValueClass)
+
+inline fun <S, T> SavedStateHandle.getValueClass(key: String, mapToValueClass: (S) -> T?): T? = get<S>(key)?.let { mapToValueClass(it) }
+inline fun <S, T> SavedStateHandle.requireValueClass(key: String, mapToValueClass: (S) -> T?): T = requireNotNull(getValueClass(key, mapToValueClass)) { createSaveStateErrorMessage(key) }
+
+fun SavedStateHandle.getDecodedString(key: String): String? = get<String>(key)?.let { URLDecoder.decode(it, "utf-8") }
 fun SavedStateHandle.requireDecodedString(key: String): String = URLDecoder.decode(requireNotNull(get<String>(key)) { createSaveStateErrorMessage(key) }, "utf-8")
-
-inline fun <T> SavedStateHandle.getValueClassString(key: String, mapToValueClass: (String) -> T?): T? = getValueClass<T, String>(key, mapToValueClass)
-inline fun <T> SavedStateHandle.requireValueClassString(key: String, mapToValueClass: (String) -> T?): T = requireValueClass<T, String>(key, mapToValueClass)
-inline fun <T> SavedStateHandle.getValueClassInt(key: String, mapToValueClass: (Int) -> T?): T? = getValueClass<T, Int>(key, mapToValueClass)
-inline fun <T> SavedStateHandle.requireValueClassInt(key: String, mapToValueClass: (Int) -> T?): T = requireValueClass<T, Int>(key, mapToValueClass)
-
-inline fun <T, S> SavedStateHandle.getValueClass(key: String, mapToValueClass: (S) -> T?): T? = get<S>(key)?.let { mapToValueClass(it) }
-inline fun <T, S> SavedStateHandle.requireValueClass(key: String, mapToValueClass: (S) -> T?): T = requireNotNull(getValueClass(key, mapToValueClass)) { createSaveStateErrorMessage(key) }
