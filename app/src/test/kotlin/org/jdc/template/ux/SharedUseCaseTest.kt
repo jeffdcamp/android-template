e5645e132c7b702c542db5d822da6bec5fe95839
jeffdcamp <jeffdcamp@gmail.com>
2023-11-20 11:51:44 -0700

    Fixed SharedUseCaseTest.kt to have TestIndividuals NOT share individuals / directoryItems / simplified TestIndividuals.directoryItems

--- app/src/test/kotlin/org/jdc/template/ux/SharedUseCaseTest.kt
+++ app/src/test/kotlin/org/jdc/template/ux/SharedUseCaseTest.kt
@@ -24,46 +24,47 @@ fun mockApplication(): Application {
 }
 
 fun mockIndividualRepository(): IndividualRepository {
+    val testIndividuals = TestIndividuals()
+
     val mockIndividualRepository = mockk<IndividualRepository>()
 
-    coEvery { mockIndividualRepository.getDirectoryListFlow() } returns flowOf(TestIndividuals.directoryItems)
+    coEvery { mockIndividualRepository.getDirectoryListFlow() } returns flowOf(testIndividuals.directoryItems)
 
     coEvery { mockIndividualRepository.getIndividual(any<IndividualId>()) } coAnswers {
         val individualId = IndividualId(firstArg<String>()) // mockk does not maintain inline/value class
-        TestIndividuals.individuals.firstOrNull { it.id == individualId }
+        testIndividuals.individuals.firstOrNull { it.id == individualId }
     }
     coEvery { mockIndividualRepository.getIndividualFlow(any<IndividualId>()) } coAnswers {
         val individualId = IndividualId(firstArg<String>()) // mockk does not maintain inline/value class
-        flowOf(TestIndividuals.individuals.firstOrNull { it.id == individualId })
+        flowOf(testIndividuals.individuals.firstOrNull { it.id == individualId })
     }
     coEvery { mockIndividualRepository.deleteIndividual(any<IndividualId>()) } coAnswers {
         val individualId = IndividualId(firstArg<String>()) // mockk does not maintain inline/value class
-        TestIndividuals.individuals.removeAll { it.id == individualId }
-        TestIndividuals.directoryItems.removeAll { it.individualId == individualId }
+        testIndividuals.individuals.removeAll { it.id == individualId }
     }
     coEvery { mockIndividualRepository.saveIndividual(any<Individual>()) } coAnswers {
         val individual = firstArg<Individual>()
-        val directoryItem = DirectoryItemEntityView(individual.id, individual.firstName, individual.lastName)
-        TestIndividuals.directoryItems.add(directoryItem)
-        TestIndividuals.individuals.add(individual)
+        testIndividuals.individuals.add(individual)
     }
 
     return mockIndividualRepository
 }
 
-object TestIndividuals {
-    val individualId1 = IndividualId("1")
-    val individual1 = Individual(id = individualId1, firstName = FirstName("Jeff"), lastName = LastName("Campbell"))
-    val individualId2 = IndividualId("2")
-    val individual2 = Individual(id = individualId2, firstName = FirstName("Mark"), lastName = LastName("Brown"))
-
+class TestIndividuals {
     val individuals: MutableList<Individual> = mutableListOf(
         individual1,
         individual2,
     )
 
-    val directoryItems: MutableList<DirectoryItemEntityView> = mutableListOf(
-        DirectoryItemEntityView(individualId1, individual1.firstName, individual1.lastName),
-        DirectoryItemEntityView(individualId2, individual2.firstName, individual2.lastName)
-    )
+    val directoryItems: List<DirectoryItemEntityView>
+        get() {
+            return individuals.map { DirectoryItemEntityView(it.id, it.firstName, it.lastName) }
+        }
+
+    companion object {
+        val individualId1 = IndividualId("1")
+        val individual1 = Individual(id = individualId1, firstName = FirstName("Jeff"), lastName = LastName("Campbell"))
+        val individualId2 = IndividualId("2")
+        val individual2 = Individual(id = individualId2, firstName = FirstName("Mark"), lastName = LastName("Brown"))
+    }
 }
