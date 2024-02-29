8169fdb54624c951f236d5c6d21455acb44fe2a0
jeffdcamp <jeffdcamp@gmail.com>
2024-02-29 15:23:36 -0700

    Changed Id value classes to not allow null value / added toString() to String value classes

--- app/src/main/kotlin/org/jdc/template/model/domain/inline/DomainInlineValue.kt
+++ app/src/main/kotlin/org/jdc/template/model/domain/inline/DomainInlineValue.kt
@@ -3,22 +3,42 @@ package org.jdc.template.model.domain.inline
 import kotlinx.datetime.Instant
 
 @JvmInline
-value class IndividualId(val value: String)
+value class IndividualId(val value: String) {
+    init {
+        require(value.isNotBlank()) { "IndividualId cannot have a empty value" }
+    }
+
+    override fun toString(): String = value
+}
 
 @JvmInline
-value class HouseholdId(val value: String)
+value class HouseholdId(val value: String) {
+    init {
+        require(value.isNotBlank()) { "HouseholdId cannot have a empty value" }
+    }
+
+    override fun toString(): String = value
+}
 
 @JvmInline
-value class FirstName(val value: String)
+value class FirstName(val value: String) {
+    override fun toString(): String = value
+}
 
 @JvmInline
-value class LastName(val value: String)
+value class LastName(val value: String) {
+    override fun toString(): String = value
+}
 
 @JvmInline
-value class Phone(val value: String)
+value class Phone(val value: String) {
+    override fun toString(): String = value
+}
 
 @JvmInline
-value class Email(val value: String)
+value class Email(val value: String) {
+    override fun toString(): String = value
+}
 
 @JvmInline
 value class CreatedTime(val value: Instant)
