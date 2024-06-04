75875448ff7d6661adcbab549e21a0b4af12f081
jeffdcamp <jeffdcamp@gmail.com>
2024-06-04 13:54:03 -0600

    Changed Previews.kt to use pre-built in Previews from Google

--- app/src/main/kotlin/org/jdc/template/ui/compose/Previews.kt
+++ app/src/main/kotlin/org/jdc/template/ui/compose/Previews.kt
@@ -1,21 +1,11 @@
 package org.jdc.template.ui.compose
 
-import android.content.res.Configuration
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 
-@PreviewLightDarkDevices
+@PreviewLightDark
 annotation class PreviewDefault
 
-@PreviewLightDarkDevices
-@PreviewMultiSizeDevices
+@PreviewLightDark
+@PreviewScreenSizes
 annotation class PreviewAll
-
-@Preview(group = "Light", uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL, showBackground = true)
-@Preview(group = "Dark", uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL, showBackground = true)
-annotation class PreviewLightDarkDevices
-
-@Preview(group = "Phone", device = "spec:shape=Normal,width=640,height=360,unit=dp,dpi=480", name = "Phone Landscape", showBackground = true)
-@Preview(group = "Phone", device = "spec:shape=Normal,width=360,height=640,unit=dp,dpi=480", name = "Phone Portrait", showBackground = true)
-@Preview(group = "Tablet", device = "spec:shape=Normal,width=1280,height=800,unit=dp,dpi=480", name = "Tablet Landscape", showBackground = true)
-@Preview(group = "Tablet", device = "spec:shape=Normal,width=800,height=1280,unit=dp,dpi=480", name = "Tablet Portrait", showBackground = true)
-annotation class PreviewMultiSizeDevices
