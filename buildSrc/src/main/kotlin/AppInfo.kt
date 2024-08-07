ca1602bd3ede05e798310594f4ae34fa15ef82fb
jeffdcamp <jeffdcamp@gmail.com>
2024-08-07 16:52:25 -0600

    Min SDK 35

--- buildSrc/src/main/kotlin/AppInfo.kt
+++ buildSrc/src/main/kotlin/AppInfo.kt
@@ -11,7 +11,7 @@ object AppInfo {
 
     object AndroidSdk {
         const val MIN = 21
-        const val COMPILE = 34
-        const val TARGET = 34
+        const val COMPILE = 35
+        const val TARGET = COMPILE
     }
 }
