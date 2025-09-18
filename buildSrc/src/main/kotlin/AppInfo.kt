b22fc751339f2cd5fda52ef6ffacb578688a007d
jeffdcamp <jeffdcamp@gmail.com>
2025-09-18 11:37:35 -0600

    Version updates

--- buildSrc/src/main/kotlin/AppInfo.kt
+++ buildSrc/src/main/kotlin/AppInfo.kt
@@ -6,7 +6,7 @@ object AppInfo {
     // Manifest version information
     object Version {
         private const val SEMANTIC_VERSION = "1.0.0"
-        val VERSION_CODE = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 0
+        val VERSION_CODE = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 1 // version code must be > 0
         val BUILD_NUMBER = System.getenv("BUILD_NUMBER") ?: 0
         val NAME = "$SEMANTIC_VERSION-($VERSION_CODE.$BUILD_NUMBER)"
     }
